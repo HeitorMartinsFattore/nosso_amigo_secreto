618b0848c24d97e7b04ef48031881f5cef1ec6bb
HeitorMartinsFattore <projetos.heitor@hotmail.com>
2018-02-01 22:32:10 -0200

    services

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -1,18 +1,59 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe CampaignMailer, type: :mailer do
-  describe "raffle" do
-    let(:mail) { CampaignMailer.raffle }
+describe RaffleService do
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Raffle")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
+  before :each do
+    @campaign = create(:campaign, status: :pending)
   end
 
+  describe '#call' do
+    context "when has more then two members" do
+      before(:each) do
+        create(:member, campaign: @campaign)
+        create(:member, campaign: @campaign)
+        create(:member, campaign: @campaign)
+        @campaign.reload
+
+        @results = RaffleService.new(@campaign).call
+      end
+
+      it "results is a hash" do
+        expect(@results.class).to eq(Hash)
+      end
+
+      it "all members are in results as a member" do
+        result_members = @results.map {|r| r.first}
+        expect(result_members.sort).to eq(@campaign.members.sort)
+      end
+
+      it "all member are in results as a friend" do
+        result_friends = @results.map {|r| r.last}
+        expect(result_friends.sort).to eq(@campaign.members.sort)
+      end
+
+      it "a member don't get yourself" do
+        @results.each do |r|
+          expect(r.first).not_to eq(r.last)
+        end
+      end
+
+      it "a member x don't get a member y that get the member x" do
+        # Desafio
+      end
+
+    end
+
+    context "when don't has more then two members" do
+      before(:each) do
+        create(:member, campaign: @campaign)
+        @campaign.reload
+
+        @response = RaffleService.new(@campaign).call
+      end
+
+      it "raise error" do
+        expect(@response).to eql(false)
+      end
+    end
+  end
 end
